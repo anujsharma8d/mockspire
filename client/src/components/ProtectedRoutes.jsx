12ab916db3e932288fb4a77606e352ebaf2fed8d
Anuj Sharma <anujsharma8d@gmail.com>
2026-09-03 19:48:51 +0530

    fix: validate JWT before allowing protected routes

--- client/src/components/ProtectedRoutes.jsx
+++ client/src/components/ProtectedRoutes.jsx
@@ -1,12 +1,53 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 import { Navigate } from 'react-router-dom';
+import api from '../api/axios';
 
 const ProtectedRoutes = ({children}) => {
+    const [loading, setLoading] = useState(true)
+    const [authenticated, setAuthenticated] = useState(false)
 
-    const token = localStorage.getItem("token");
-    if(!token){
-        return <Navigate to={"/login"} replace/>
+    useEffect(() => {
+        const verifyToken= async()=>{
+            const token = localStorage.getItem("token");
+            
+            if(!token){
+                setAuthenticated(false);
+                setLoading(false);
+                return;
+            }
+
+            try{
+                await api.get("api/auth/verify",{
+                    headers:{
+                        Authorization:`Bearer ${token}`
+                    }
+                })
+
+                setAuthenticated(true)
+            }catch (error) {
+                console.log("Invalid or expired token");
+
+                localStorage.removeItem("token");
+
+                setAuthenticated(false);
+            } finally{
+                setLoading(false);
+            }
+
+        }
+
+        verifyToken();
+
+    }, [])
+
+    if (loading) {
+        return <div>Loading...</div>;
     }
+
+    if (!authenticated) {
+        return <Navigate to="/login" replace />;
+    }
+
     return children;
 }
 
